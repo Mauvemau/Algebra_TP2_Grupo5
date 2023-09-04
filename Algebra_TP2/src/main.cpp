ed575805b9ac39443165b0b4c136fe1a8e9f0a14
Mau <mauvepinktree@gmail.com>
2023-09-04 12:18:05 -0300

    Tidied up code

--- Algebra_TP2/src/main.cpp
+++ Algebra_TP2/src/main.cpp
@@ -1,67 +1,71 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+struct Vector {
+    Vector3 vectorStart;
+    Vector3 vectorEnd;
+};
+
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+Vector v1;
+Vector v2;
+Vector v3;
+
+Camera3D camera;
+
+void initVectors();
+
+void draw();
+void update();
+void init();
+void uninit();
+
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    init();
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    update();
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    uninit();
     return 0;
+}
+
+void initVectors() {
+
+}
+
+void draw() {
+    BeginDrawing();
+        ClearBackground(BLACK);
+
+        BeginMode3D(camera);
+
+
+
+        EndMode3D();
+    EndDrawing();
+}
+
+void update() {
+    while (!WindowShouldClose()) {
+
+
+        draw();
+    }
+}
+
+void init() {
+    InitWindow(screenWidth, screenHeight, "Algebra_TP2");
+    SetTargetFPS(60);
+
+    camera.position = Vector3{ 0.0f, 0.0f, 20.0f };
+    camera.target = Vector3{ 0.0f, 5.0f, 0.0f };
+    camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+}
+
+void uninit() {
+    CloseWindow();
 }
